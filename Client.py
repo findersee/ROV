8aba19581fdf01733fa44a34803792d03bd8b1cc
Finder <aki.kuusela@findersee.fi>
2021-04-22 22:27:49 +0300

    GUI Commit

--- Client.py
+++ Client.py
@@ -4,20 +4,25 @@ HOST = '127.0.0.1'
 PORT = 1234
 
 def menu():
-    
+    print("Test")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #s.connect((HOST, PORT))
-    while True:
-        Test= input('Select setting \r\n 0:Power \r\n 1:Depth \r\n')
-        #s.sendall(bytes(Test,'utf-8'))
-        #data = s.recv(1024)
-        #print('Received', repr(data))    
-        if Test == 'q':
-            break
-    #s.sendall(b'Hella world')
-    
-    #s.sendall(b'Test')
-    
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #s.connect((HOST, PORT))
+        print("TEST")
+        while True:
+            Test= input('Select setting \r\n 0:Power \r\n 1:Depth \r\n')
+            #s.sendall(bytes(Test,'utf-8'))
+            #data = s.recv(1024)
+            #print('Received', repr(data))    
+            if Test == 'q':
+                break
+        #s.sendall(b'Hella world')
+        
+        #s.sendall(b'Test')
+        
 
-#print('Received', repr(data))
+    #print('Received', repr(data))
+    
+if __name__== "__main__":
+    exit(main())
