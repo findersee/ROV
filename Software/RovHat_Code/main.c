abc5144514745e6d5b20e81efc84be9a6cc87b0e
Aki Kuusela <git@findersee.fi>
2023-02-10 22:55:58 +0200

    Added pico-ads1115

--- Software/RovHat_Code/main.c
+++ Software/RovHat_Code/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "ads1115.h"
+
+#define I2C_PORT i2c0
+#define I2C_FREQ 400000
+#define ADS1115_I2C_ADDR 0x48
+const uint8_t SDA_PIN = 20;
+const uint8_t SCL_PIN = 21;
+
+struct ads1115_adc adc;
+
+
+
 
 int main() {
 
@@ -11,6 +23,17 @@ int main() {
 
     // Initialize chosen serial port
     stdio_init_all();
+
+    // Initialise I2C
+    i2c_init(I2C_PORT, I2C_FREQ);
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN);
+
+    // Initialise ADC
+    ads1115_init(I2C_PORT, ADS1115_I2C_ADDR, &adc);
+
 
     // Loop forever
     while (true) {
